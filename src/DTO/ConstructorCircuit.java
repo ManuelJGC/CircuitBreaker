dcdc3b559286585b1e016aaeb04da568d398ad56
Manuel Jesús González Carmona <mjgonzcar@gmail.com>
2021-12-21 13:14:12 +0100

    Initial commit

---
+++ src/DTO/ConstructorCircuit.java
@@ -0,0 +1,18 @@
+package DTO;
+
+public class ConstructorCircuit {
+
+    private Integer nintentosfallidos;
+    private Integer nintentosok;
+    private Integer limiteerror;
+    private Integer nintentosmax;
+    private int nintentosactual;
+    private String estado;
+    
+    if (this.nintentosfallidos>=)
+    for (this.nintentosactual=1; this.nintentosactual<this.nintentosmax; ++this.nintentosactual){
+        
+    };
+        
+    
+}
